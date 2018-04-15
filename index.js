2e1fa8b1b70bdd835e025c123ea2b19ece623a8c
victor hazbun <victorhazbun87@gmail.com>
2018-04-15 17:49:23 -0500

    Creating & List Elasticsearch Indices

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ const fullUrl = (path = '') => {
   return url + path.replace(/^\/*/, '');
 };
 
+const handleResponse = (err, res, body) => {
+  if (program.json) {
+    console.log(JSON.stringify(err || body));
+  } else {
+    if (err) throw err;
+    console.log(body);
+  }
+};
+
 program
   .version(pkg.version)
   .description(pkg.description)
@@ -30,6 +39,48 @@ program
   .command('url [path]')
   .description('generate the URL for the options and path (default is /)')
   .action((path = '/') => console.log(fullUrl(path)));
+
+program
+  .command('get [path]')
+  .description('perform an HTTP GET request for path (default is /)')
+  .action((path = '/') => {
+    const options = {
+      url: fullUrl(path),
+      json: program.json,
+    };
+    request(options, (err, res, body) => {
+      if (program.json) {
+        console.log(JSON.stringify(err || body));
+      } else {
+        if (err) throw err;
+        console.log(body);
+      }
+    });
+  });
+
+program
+  .command('create-index')
+  .description('create an index')
+  .action(() => {
+    if (!program.index) {
+      const msg = 'No index specified! Use --index <name>';
+      if (!program.json) throw Error(msg);
+      console.log(JSON.stringify({error: msg}));
+      return;
+    }
+
+    request.put(fullUrl(), handleResponse);
+  });
+
+program
+  .command('list-indices')
+  .alias('li')
+  .description('get a list of indices in this cluster')
+  .action(() => {
+    const path = program.json ? '_all' : '_cat/indices?v';
+    request({url: fullUrl(path), json: program.json}, handleResponse);
+  });
+
 
 program.parse(process.argv);
 

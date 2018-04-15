ae456131730e8d21d9ee05f887876cc432da516f
victor hazbun <victorhazbun87@gmail.com>
2018-04-15 17:37:00 -0500

    Creating a Basic Command-Line Program with Commander

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const fs = require('fs');
+const request = require('request');
+const program = require('commander');
+const pkg = require('./package.json');
+
+const fullUrl = (path = '') => {
+  let url = `http://${program.host}:${program.port}/`;
+  if (program.index) {
+    url += program.index + '/';
+    if (program.type) {
+      url += program.type + '/';
+    }
+  }
+  return url + path.replace(/^\/*/, '');
+};
+
+program
+  .version(pkg.version)
+  .description(pkg.description)
+  .usage('[options] <command> [...]')
+  .option('-o, --host <hostname>', 'hostname [localhost]', 'localhost')
+  .option('-p, --port <number>', 'port number [9200]', '9200')
+  .option('-j, --json', 'format output as JSON')
+  .option('-i, --index <name>', 'which index to use')
+  .option('-t, --type <type>', 'default type for bulk operations');
+
+program
+  .command('url [path]')
+  .description('generate the URL for the options and path (default is /)')
+  .action((path = '/') => console.log(fullUrl(path)));
+
+program.parse(process.argv);
+
+if (!program.args.filter(arg => typeof arg === 'object').length) {
+  program.help();
+}
